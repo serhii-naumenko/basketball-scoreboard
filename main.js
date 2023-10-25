db1d15b5efae50e8c9c264d03a9282bfef0f61a4
Serhii Naumenko <s.naumenko.miy@gmail.com>
2023-10-25 23:06:02 +0300

    added logic for pause and continue of timer

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const homeEl = document.getElementById('scoreHome');
 const guestEl = document.getElementById('scoreGuest');
-const timeEl = document.getElementById("time");
+const timeEl = document.getElementById('time');
 const buttonStartEl = document.getElementById('btn-start')
+const pauseEl = document.getElementById('pause');
+const continueEl = document.getElementById('continue');
+let period;
 
 let scoreHome = 0;
 let scoreGuest = 0;
 let time = 0;
-let isTimer = false;
 
 function increment(side, amount) {
   if (side === 'home') {
@@ -18,20 +20,22 @@ function increment(side, amount) {
   }
 }
 
-function gameStart() {
-  buttonStartEl.textContent = 'The game is going';
-  buttonStartEl.disabled = true;
-  time = 0;
-  timeEl.textContent = '00:00';
-  const period = setInterval(() => {
+function startTimer() {
+  period = setInterval(() => {
     time += 1;
     timeEl.textContent = convertTime(time);
   }, 1000);
-  if (isTimer) {
-    clearInterval(period);
-    period();
-  }
-  isTimer = true;
+  pauseEl.disabled = false;
+  continueEl.disabled = true;
+  buttonStartEl.textContent = 'The game is going';
+  buttonStartEl.style.color = '#022dc7';
+}
+
+function gameStart() {
+  buttonStartEl.disabled = true;
+  time = 0;
+  timeEl.textContent = '00:00';
+  startTimer();
 }
 
 function convertTime(currentTime) {
@@ -40,4 +44,12 @@ function convertTime(currentTime) {
   let seconds = currentTime - minutes * 60;
   if (seconds < 10) seconds = '0' + seconds;
   return minutes + ':' + seconds;
+}
+
+function pauseTimer() {
+  clearInterval(period);
+  pauseEl.disabled = true;
+  continueEl.disabled = false;
+  buttonStartEl.textContent = 'P A U S E';
+  buttonStartEl.style.color = 'red';
 }
